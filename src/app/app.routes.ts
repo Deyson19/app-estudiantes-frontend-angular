a0efd1b61a4a06404c723d69af0e9dd0f6da0886
Deyson Venté <deiandresvent@hotmail.com>
2024-05-31 17:23:02 -0500

    rutas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { UpsertComponent } from './pages/upsert/upsert.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
 
-export const routes: Routes = [];
+//*Configurar rutas
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'home',
+        component: ListadoComponent,
+      },
+      {
+        path: 'nuevo',
+        component: UpsertComponent,
+      },
+      {
+        path: 'detalle/:id',
+        component: DetalleComponent,
+      },
+      {
+        path: 'editar/:id',
+        component: UpsertComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
